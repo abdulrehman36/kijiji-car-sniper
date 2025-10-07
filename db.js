41f7a26eb5a7b15e23c0fe5503e67174e3fb528c
abdulrehman36 <123913847+abdulrehman36@users.noreply.github.com>
2025-10-07 16:04:17 -0600

    added some print messages to check if im connecting to sql server

--- db.js
+++ db.js
@@ -1,16 +1,26 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const mysql = require('mysql2');
+
+console.log('Connecting to MySQL...');
+console.log('Host:', process.env.DB_HOST);
+console.log('Database:', process.env.DB_NAME);
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect(err => {
-  if (err) throw err;
-  console.log('Connected to MySQL');
+  if (err) {
+    console.error('Database connection failed:', err.message);
+  } else {
+    console.log('Connected to MySQL');
+  }
 });
 
 module.exports = db;
